e5ece49c7a637584bfd9af583ce08843a25f5835
jbwwboom <justinvdboomen@gmail.com>
2018-09-24 11:32:44 +0200

    Implemented Picasso for Image loading from URLs.

--- app/src/main/java/com/example/justi/contactcardapp/UserAdapter.java
+++ app/src/main/java/com/example/justi/contactcardapp/UserAdapter.java
@@ -1,12 +1,8 @@
 package com.example.justi.contactcardapp;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.justi.contactcardapp.models.RandomUser;
+import com.squareup.picasso.Picasso;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 
 public class UserAdapter extends ArrayAdapter<RandomUser> {
@@ -45,33 +41,8 @@ public class UserAdapter extends ArrayAdapter<RandomUser> {
 
         name.setText( user.getLogin().getUsername() );
 
-        new DownloadImageTask(image).execute(user.getPicture().getLarge());
+        Picasso.get().load(user.getPicture().getLarge()).into(image);
 
         return convertView;
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
     }
 }
